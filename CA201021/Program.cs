027ee50aac1f51f3a4b0d7c4098afbe78b9ed1a2
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-10-21 11:10:53 +0200

    Add project files.

---
+++ CA201021/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CA201021
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main()
+        {
+            var t = new int[10];
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = rnd.Next(10, 100);
+                Console.Write($"{t[i]}, ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
